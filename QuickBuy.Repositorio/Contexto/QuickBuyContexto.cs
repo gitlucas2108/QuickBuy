ef5385cb5610bd5fcf6da79f5de43f659ff4d4a3
lcstrinda_dev <lcstrinda_dev@hotmail.com>
2022-05-17 20:32:55 -0300

    aula 170522

--- QuickBuy.Repositorio/Contexto/QuickBuyContexto.cs
+++ QuickBuy.Repositorio/Contexto/QuickBuyContexto.cs
@@ -27,8 +27,32 @@ namespace QuickBuy.Repositorio.Contexto
             modelBuilder.ApplyConfiguration(new ItemPedidoConfiguration());
             //modelBuilder.ApplyConfiguration(new FormaPagamentoConfiguration());
 
+            //Teste de carga em tabela
+            modelBuilder.Entity<Produto>().HasData(
+                                        new Produto()
+                                        {
+                                            Id = 1,
+                                            Nome = "PRODUTO 1",
+                                            Descricao = "Produto cadastrado pelo QuickBuy.",
+                                            Preco = 10
+                                        },
+                                        new Produto()
+                                        {
+                                            Id = 2,
+                                            Nome = "PRODUTO 2",
+                                            Descricao = "Produto cadastrado pelo QuickBuy.",
+                                            Preco = 20
+                                        }, new Produto()
+                                        {
+                                            Id = 3,
+                                            Nome = "PRODUTO 3",
+                                            Descricao = "Produto cadastrado pelo QuickBuy.",
+                                            Preco = 30.30M
+                                        }
+                                        );
+
             base.OnModelCreating(modelBuilder);
         }
 
-    }
+    } 
 }
---
+++ QuickBuy.Repositorio/Migrations/20220517225211_CargaProduto.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace QuickBuy.Repositorio.Migrations
+{
+    public partial class CargaProduto : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Produtos",
+                columns: new[] { "Id", "Descricao", "Nome", "Preco" },
+                values: new object[] { 1, "Produto cadastrado pelo QuickBuy.", "PRODUTO 1", 10.0 });
+
+            migrationBuilder.InsertData(
+                table: "Produtos",
+                columns: new[] { "Id", "Descricao", "Nome", "Preco" },
+                values: new object[] { 2, "Produto cadastrado pelo QuickBuy.", "PRODUTO 2", 20.0 });
+
+            migrationBuilder.InsertData(
+                table: "Produtos",
+                columns: new[] { "Id", "Descricao", "Nome", "Preco" },
+                values: new object[] { 3, "Produto cadastrado pelo QuickBuy.", "PRODUTO 3", 30.300000000000001 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Produtos",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Produtos",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Produtos",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
